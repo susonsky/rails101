19a8d19c856607cfece83f886fd187f991f2bf70
苏文旭 <susonsky2018@gmail.com>
2017-03-21 23:32:51 +0800

    add gorups

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    flash[:notice] = " 早安！您好！"
+  ##  flash[:notice] = " 早安！您好！"
     flash[:alert] = "晚安，该睡了"
     flash[:wwarning]="这是warning！"
   end
