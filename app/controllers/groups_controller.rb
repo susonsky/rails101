f8f571a3c452878c8abe904563aedb465a8d59b4
苏文旭 <susonsky2018@gmail.com>
2017-03-22 00:40:14 +0800

    add edit&destroy in the controllers

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,7 +10,7 @@ class GroupsController < ApplicationController
   def edit
    @group = Group.find(params[:id])
   end
-  
+
   def new
     @group = Group.new
   end
@@ -20,6 +20,20 @@ class GroupsController < ApplicationController
 
       redirect_to groups_path
   end
+
+  def update
+     @group = Group.find(params[:id])
+
+     @group.update(group_params)
+
+     redirect_to groups_path, notice: "Update Success"
+   end
+   def destroy
+        @group = Group.find(params[:id])
+        @group.destroy
+        flash[:alert] = "Group deleted"
+        redirect_to groups_path
+      end
 
   private
 
