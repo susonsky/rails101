78de1dd770767091726cee1fb0b964a1f7723db9
苏文旭 <susonsky2018@gmail.com>
2017-03-22 23:10:20 +0800

    change word

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ApplicationRecord
-##  validates :title, presence: ture
+##  validates :title, presence: ture （拼写错误，是坑）
+##  end
+class Group < ApplicationRecord
+ validates :title, presence: true
 end
